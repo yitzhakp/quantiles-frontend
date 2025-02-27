9c90989e0ae80bb01c2939cbb88c955bf3ef591a
Yitzhak P. J. <yitzhakp@uninorte.edu.co>
2025-02-27 13:09:23 -0500

    run build

---
+++ .netlify/functions-internal/ssr/chunks/generic_BMjowC1n.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_Cs0q-Og0.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/ssr/chunks/index_C5hgQKM2.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_B8L-WkXE.mjs');
+
+export { page };
